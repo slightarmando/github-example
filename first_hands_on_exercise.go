5b26f56482fe35e8d231702511a995d7d974e019
Armando Correa <armandocorrea@Armandos-MacBook-Pro.local>
2022-12-30 14:26:55 -0800

    well done

--- first_hands_on_exercise.go
+++ first_hands_on_exercise.go
@@ -2,12 +2,23 @@ package main
 
 import "fmt"
 
-var x = 42
-var y = "James Bond"
-var z = true
+var a int
+
+type hotdog float32
+
+var b hotdog
+
+var y int
 
 func main() {
-	s := fmt.Sprintf("%v\t%v\t%v", y, x, z)
-	fmt.Println(s)
+	a = 42
+	b = 19.0
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
+	fmt.Printf("%T\n", b)
+	fmt.Println(b)
+	y = int(b)
+	fmt.Printf("%T\n", y)
+	fmt.Println(y)
 
 }
